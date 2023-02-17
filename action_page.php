e40392ba6d5ace6c182ee0349f7622185644ba85
walmartwizard <123500221+walmartwizard@users.noreply.github.com>
2023-02-16 21:09:25 -0500

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -2,7 +2,7 @@
 if (isset($_POST['Email'])) {
 
     // EDIT THE FOLLOWING TWO LINES:
-    $email_to = "you@yourdomain.com";
+    $email_to = "chihuahuasilas@gmail.com";
     $email_subject = "New form submissions";
 
     function problem($error)
@@ -69,7 +69,7 @@ if (isset($_POST['Email'])) {
 
     <!-- INCLUDE YOUR SUCCESS MESSAGE BELOW -->
 
-    Thanks for getting in touch. We'll get back to you soon.
+    Thanks for getting in touch. I'll get back to you soon.
 
 <?php
 }

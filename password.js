c16e307541f2214c7be899e6db36c61f80a26d8a
walmartwizard <123500221+walmartwizard@users.noreply.github.com>
2023-10-23 00:12:04 -0400

    Create password.js

---
+++ password.js
@@ -0,0 +1,7 @@
+    function checkPswd() {
+      var confirmPassword = "YEEZY";
+      var password = document.getElementById("pswd").value;
+      if (password == confirmPassword) {
+        window.location = "welkom.html";
+      }
+    }
